bc9f752f904dd74407cef49cd5ec73a694ced4a9
Angelo Chiu <achiu@vendasta.com>
2019-06-27 16:26:54 -0600

    add edit click event to show item details
    STEP-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ContentsComponent } from './contents/contents.component';
 
@@ -12,7 +14,8 @@ import { ITEMS } from './mock-items';
     ContentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contents/contents.component.ts
+++ src/app/contents/contents.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Item } from '../item';
 import { ITEMS } from '../mock-items';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-contents',
@@ -12,16 +13,21 @@ export class ContentsComponent implements OnInit {
 
   items = ITEMS;
 
-  content: Item = {
-    id: 1,
-    name: 'apple',
-    quantity: 5,
-    description: 'brandless red'
-  };
+  // content: Item = {
+  //   id: 1,
+  //   name: 'apple',
+  //   quantity: 5,
+  //   description: 'brandless red'
+  // };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  editingItem: Content;
+  editItem(content: Content): void {
+    this.editingItem = content;    
   }
 
 }
