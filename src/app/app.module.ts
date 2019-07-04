eba037d75c1ec075f7259f0cd3802dcc3d2aefed
Angelo Chiu <achiu@vendasta.com>
2019-07-04 15:21:02 -0600

    added messages component
    STEP-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { EditItemDetailComponent } from "./edit-item-detail/edit-item-detail.com
 
 import { MatTableModule } from "@angular/material";
 import { CdkTableModule } from "@angular/cdk/table";
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentsComponent, EditItemDetailComponent],
+  declarations: [AppComponent, ContentsComponent, EditItemDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule, MatTableModule, CdkTableModule],
   providers: [],
   bootstrap: [AppComponent]
