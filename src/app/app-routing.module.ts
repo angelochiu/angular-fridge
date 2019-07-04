e12178c20907f8391b21dc84332ad5a2a0c1fa96
Angelo Chiu <achiu@vendasta.com>
2019-07-04 16:39:00 -0600

    added routing code
    STEP-5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { ContentsComponent } from "./contents/contents.component";
+
+const routes: Routes = [{ path: "contents", component: ContentsComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
