6043696cf1ed3dae9cef14f484107d13af8743dc
Angelo Chiu <achiu@vendasta.com>
2019-07-04 15:44:50 -0600

    completed message service
    STEP-4

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+import { MessageService } from "./message.service";
 
 import { Item } from "./item";
 import { ITEMS } from "./mock-items";
@@ -8,9 +9,11 @@ import { ITEMS } from "./mock-items";
   providedIn: "root"
 })
 export class ItemService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getItems(): Observable<Item[]> {
+    // TODO: send he message _after_ fetching the items
+    this.messageService.add("ItemService: item fetched");
     return of(ITEMS);
   }
 }
